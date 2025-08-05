ebd245af966da7ecf3f55f8f4da61d94289a5cbd
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2025-08-05 08:25:14 -0500

    Able to get form input

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const unit = document.getElementById('unit-select');
 const loading = document.getElementById('loading');
 const display = document.getElementById('weather-display');
 
-form.addEventListener('click', formClick);
+form.addEventListener('submit', formClick);
 
 function formClick(event) {
-    
     event.preventDefault();
+    const location = input.value.trim();
+    const selectedUnit = unit.value;
+
+    console.log("Loaction: " + location);
+    console.log("Unit: " + selectedUnit);
 }
